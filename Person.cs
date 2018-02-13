2f1eba9ff4abef7c0aaa9eb7cc235d7e6774dad8
Bryan Leonard <bleonard@acrtinc.com>
2018-02-13 17:00:16 -0500

    Fix build

--- Person.cs
+++ Person.cs
@@ -39,90 +39,85 @@ namespace Packt.CS7
     }
 
 
-    // Stuff prior to Chapter 6
-    public partial class OldPerson
-    {
+    //// Stuff prior to Chapter 6
+    //public partial class OldPerson
+    //{
 
 
 
-        //Prior to Chapter 6
+    //    //Prior to Chapter 6
 
-        public string Name;
-        public string FavHobby;
-        public DateTime DOB;
-        public WondersOfTheAncientWorld FavouriteAncientWonder;
-        public List<OldPerson> Children = new List<OldPerson>();
-        public const string Species = "Homo Sapien";
-        public readonly string HomePlanet = "Earth";
+    //    public string Name;
+    //    public string FavHobby;
+    //    public DateTime DOB;
+    //    public WondersOfTheAncientWorld FavouriteAncientWonder;
+    //    public List<OldPerson> Children = new List<OldPerson>();
+    //    public const string Species = "Homo Sapien";
+    //    public readonly string HomePlanet = "Earth";
 
-        public readonly DateTime Instantiated; // constructors 
-        public OldPerson()
-        {
-            // set default values for fields 
-            // including read-only fields 
-            Name = "Unknown";
-            Instantiated = DateTime.Now;
-        }
-
-
-        public OldPerson(string initialName)
-        {
-            Name = initialName;
-            Instantiated = DateTime.Now;
-        }
+    //    public readonly DateTime Instantiated; // constructors 
+    //    public OldPerson()
+    //    {
+    //        // set default values for fields 
+    //        // including read-only fields 
+    //        Name = "Unknown";
+    //        Instantiated = DateTime.Now;
+    //    }
 
 
-        // methods 
-        public void WriteToConsole()
-        {
-            WriteLine($"{Name} was born on {DOB:dddd, d MMMM yyyy}");
-        }
-
-        public string GetOrigin()
-        {
-            return $"{Name} was born on {HomePlanet}";
-        }
-
-        public string OptionalParameters(string command = "Run!", double number = 0.0, bool active = true)
-        {
-            return $"command is {command}, number is {number}, active is {active}";
-        }
-
-        public void PassingParameters(int x, ref int y, out int z)
-        {
-            // out parameters cannot have a default  
-            // AND must be initialized inside the method 
-            z = 99;
-
-            // increment each parameter 
-            x++;
-            y++;
-            z++;
-        }
+    //    public OldPerson(string initialName)
+    //    {
+    //        Name = initialName;
+    //        Instantiated = DateTime.Now;
+    //    }
 
 
-        //***
+    //    // methods 
+    //    public void WriteToConsole()
+    //    {
+    //        WriteLine($"{Name} was born on {DOB:dddd, d MMMM yyyy}");
+    //    }
 
-        // old C# 4
-        public Tuple<string, int> GetFruitCS4()
-        {
-            return Tuple.Create("Apples", 5);
-        }
+    //    public string GetOrigin()
+    //    {
+    //        return $"{Name} was born on {HomePlanet}";
+    //    }
 
-        // new C# 7
-        public (string, int) GetFruitCS7()
-        {
-            return ("Oranges", 6);
-        }
+    //    public string OptionalParameters(string command = "Run!", double number = 0.0, bool active = true)
+    //    {
+    //        return $"command is {command}, number is {number}, active is {active}";
+    //    }
 
-        public (string Name, int Number) GetNamedFruit()
-        {
-            return (Name: "Apples", Number: 5);
-        }
+    //    public void PassingParameters(int x, ref int y, out int z)
+    //    {
+    //        // out parameters cannot have a default  
+    //        // AND must be initialized inside the method 
+    //        z = 99;
+
+    //        // increment each parameter 
+    //        x++;
+    //        y++;
+    //        z++;
+    //    }
 
 
+    //    //***
 
+    //    // old C# 4
+    //    public Tuple<string, int> GetFruitCS4()
+    //    {
+    //        return Tuple.Create("Apples", 5);
+    //    }
 
+    //    // new C# 7
+    //    public (string, int) GetFruitCS7()
+    //    {
+    //        return ("Oranges", 6);
+    //    }
 
-    }
+    //    public (string Name, int Number) GetNamedFruit()
+    //    {
+    //        return (Name: "Apples", Number: 5);
+    //    }
+    //}
 }
--- Person2.cs
+++ Person2.cs
@@ -10,57 +10,57 @@ namespace Packt.CS7
     }
 
 
-    public partial class OldPerson
-    {
+    //public partial class OldPerson
+    //{
 
-        //Prior to Chapter 6
+    //    //Prior to Chapter 6
 
-        public string Origin
-        {
-            get
-            {
-                return $"{Name} was born on {HomePlanet}.";
-            }
-        }
+    //    public string Origin
+    //    {
+    //        get
+    //        {
+    //            return $"{Name} was born on {HomePlanet}.";
+    //        }
+    //    }
 
-        public string Greeting => $"{Name} says hello!";
+    //    public string Greeting => $"{Name} says hello!";
 
-        public int Age => (int)(System.DateTime.Today.Subtract(DOB).TotalDays / 365.25);
+    //    public int Age => (int)(System.DateTime.Today.Subtract(DOB).TotalDays / 365.25);
 
-        public string FaveIceCream { get; set; }
+    //    public string FaveIceCream { get; set; }
 
-        private string favePrimaryColor;
-        public string FavePrimaryColor
-        {
-            get => favePrimaryColor;
-            //set => favePrimaryColor = value;
-            set
-            {
-                switch (value.ToLower())
-                {
-                    case "red":
-                    case "green":
-                    case "blue":
-                        favePrimaryColor = value;
-                        break;
+    //    private string favePrimaryColor;
+    //    public string FavePrimaryColor
+    //    {
+    //        get => favePrimaryColor;
+    //        //set => favePrimaryColor = value;
+    //        set
+    //        {
+    //            switch (value.ToLower())
+    //            {
+    //                case "red":
+    //                case "green":
+    //                case "blue":
+    //                    favePrimaryColor = value;
+    //                    break;
 
-                    default:
-                        throw new System.ArgumentException($"{value} is not a primary color. Choose from: red, green, blue.");
-                }
-            }
-        }
+    //                default:
+    //                    throw new System.ArgumentException($"{value} is not a primary color. Choose from: red, green, blue.");
+    //            }
+    //        }
+    //    }
 
-        // indexers - not that you'd really use this kind of thing
-        public Person this[int index]
-        {
-            get
-            {
-                return Children[index];
-            }
-            set
-            {
-                Children[index] = value;
-            }
-        }
-    }
+    //    // indexers - not that you'd really use this kind of thing
+    //    public Person this[int index]
+    //    {
+    //        get
+    //        {
+    //            return Children[index];
+    //        }
+    //        set
+    //        {
+    //            Children[index] = value;
+    //        }
+    //    }
+    //}
 }
