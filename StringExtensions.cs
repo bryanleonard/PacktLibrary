7742c815590d2c6bbbd0038f2ad02475c800024f
Bryan Leonard <bleonard@acrtinc.com>
2018-03-02 16:44:55 -0500

    End of chapter 7

---
+++ StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Packt.CS7
+{
+    public static class StringExtensions
+    //public class StringExtensions
+    // works but can be better  StringExtensions.IsValidEmail(email1)
+    // versus email1.IsValidEmail()
+    {
+        // old version doesn't use 'this'
+        public static bool IsValidEmail(this string input)
+        {
+            // use simple regular expression to check  
+            // that the input string is a valid email
+            return Regex.IsMatch(input, @"[a-zA-Z0-9\.-_]+@[a-zA-Z0-9\.-_]+");
+        }
+    }
+}
